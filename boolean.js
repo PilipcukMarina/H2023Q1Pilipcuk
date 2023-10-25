18ca671baa7c6506c6fc562d4945cb4ca74ee2fe
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-25 20:06:11 +0300

    add: weekday search

---
+++ boolean.js
@@ -0,0 +1,7 @@
+const firstNumber = 1
+const secondNumber = 1
+if (firstNumber > secondNumber)
+    console.log('число ' + firstNumber + ' больше ' + secondNumber)
+else
+    console.log('число ' + firstNumber + ' меньше ' + secondNumber)
+
